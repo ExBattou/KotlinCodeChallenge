5613cf83101f67777492a27b018df231cbdc6ed0
Adrian Narducci <adriann@bluetrailsoft.com>
2019-06-25 10:24:13 -0300

    Initial commit

---
+++ app/src/main/java/net/adriann/kotlincodechallenge/ui/MainActivity.kt
@@ -0,0 +1,47 @@
+package net.adriann.kotlincodechallenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import net.adriann.kotlincodechallenge.R
+import net.adriann.kotlincodechallenge.service.WikiApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    private var disposable: Disposable? = null
+
+    private val wikiApiServe by lazy {
+        WikiApiService.create()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_search.setOnClickListener {
+            if (edit_search.text.toString().isNotEmpty()) {
+                beginSearch(edit_search.text.toString())
+            }
+        }
+    }
+
+    private fun beginSearch(searchString: String) {
+        disposable = wikiApiServe.hitCountCheck("query", "json", "search", searchString)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result -> txt_search_result.text = "${result.query.searchinfo.totalhits} result found" },
+                { error -> Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show() }
+            )
+    }
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
+    }
+}
